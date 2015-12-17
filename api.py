03c1093a6a315db7b3b50138df523bd5c87b0f66
Eddy Hintze <eddy.hintze@gmail.com>
2015-12-16 19:52:49 -0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from flask import Flask, request
+from flask.ext.restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app, prefix='/api/v1')
+
+
+class LiveNodes(Resource):
+    def post(self):
+        # Make sure it is an authorized router
+        router = request.remote_addr
+        if router == '10.224.98.1':
+            data = request.json
+            parse_data(data)
+        else:
+            print("Data from unauthorized router")
+
+        return {'success': True}
+
+api.add_resource(LiveNodes, '/livenodes')
+
+
+def parse_data(data):
+
+    nodes = data['nodes']
+    nodes = nodes.split("\n")
+    # Remove first 2 lines because it is just the header
+    nodes.pop(0)
+    nodes.pop(0)
+
+    # name,blocked,primaryIp,routes,viaIp,viaDev,metric,lastDesc,lastRef,
+    for node in nodes:
+        print(node)
+
+    print(data['timestamp'])
+    print(len(nodes))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ router_monitor.sh
@@ -0,0 +1,13 @@
+node_list_raw=$(bmx6 -c --originators)                                                                                                                                  
+                                                                                                                                                                        
+node_list_raw=$(printf "%s" "$node_list_raw" | sed -e "s/[[:space:]]\+/,/g")                                                                                            
+node_list_raw=$(printf "%s" "$node_list_raw" | sed -e ':a;N;$!ba;s/\n/\\n/g')                                                                                           
+                                                                                                                                                                        
+node_list=$node_list_raw                                                                                                                                                
+                                                                                                                                                                        
+host="http://10.224.98.177:5000/api/v1/livenodes"                                                                                                                       
+timestamp=$(date +%s)                                                                                                                                                   
+                                                                                                                                                                        
+data={\"nodes\":\"$node_list\",\"timestamp\":\"$timestamp\"}                                                                                                            
+                                                                                                                                                                        
+curl -i -X POST -H "Content-Type: application/json" -d "$data" $host
