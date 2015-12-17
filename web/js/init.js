d1ea99423c9cac1e7b863133000a5df0571ce5ca
Eddy Hintze <eddy.hintze@gmail.com>
2015-12-17 12:35:56 -0500

    Added static files. Time formatted

--- web/js/init.js
+++ web/js/init.js
@@ -3,17 +3,26 @@ $(function(){
     var template_nodeList = Handlebars.compile($("#nodeList").html());
     Handlebars.registerPartial('nodeRow', $("#nodeRow").html());
 
-    $.getJSON( "http://localhost:5000/api/v1/nodes", function( json ) {
+    $.getJSON( "/api/v1/nodes", function( json ) {
         console.log(json);
+        
+        json.data.global_last_seen_formatted = formatTime(json.data.global_last_seen);
+
         $.each(json.data.nodes, function( index, value ) {
             // Check if node is connected
-            value.status = 0;  // Default value
+            value.status = 'error';  // Default value
             if(value.lastSeen === json.data.global_last_seen){
-                value.status = 1;
+                value.status = 'success';
             }
+            // Format last_seen
+            value.last_seen_formatted = formatTime(value.lastSeen)
         });
         $('#list').html(template_nodeList(json.data))
     });
 
+    function formatTime(epoch){
+        var formattedTime = moment(epoch, 'X');
+        return formattedTime.format('llll');
+    }
 
 });
